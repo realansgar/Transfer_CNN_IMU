b438efb5532a9323a57e27c68a306b634fcaae14
oskar.zeino <oskar.zeino@tu-dortmund.de>
2020-09-01 17:23:43 +0200

    Training/Evaluation progress
    Former-commit-id: 8679645d6a6ccf7a7ab6cd5775da0233c2e4157b

--- train.py
+++ train.py
@@ -1,84 +1,79 @@
 import torch
-from torch import nn, optim
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 import time
 from tqdm import tqdm
 from CNN import SimpleCNN
 from datasets import HARWindows
 import preprocessing
-from config import *
+from config import DEVICE
 
+class Trainer():
+  def __init__(self, config_dict):
+    list(map(lambda item: setattr(self, *item), config_dict.items()))
 
-def process_batch(batch, net, criterion, optimizer=None):
-  data_batch, label_batch = data
-  if USE_CUDA:
-    data_batch = data_batch.cuda()
-    label_batch = label_batch.cuda()
-      
-  # forward and backward pass
-  if optimizer is not None:
-    optimizer.zero_grad()
-  out_batch = net(data_batch)
-  loss = criterion(out_batch, label_batch)
-  if optimizer is not None:
-    loss.backward()
-    optimizer.step()
+  def process_batch(self, batch, optimize=True):
+    data_batch, label_batch = batch
+    data_batch = data_batch.to(DEVICE)
+    label_batch = label_batch.to(DEVICE)
+        
+    # forward and backward pass
+    if optimize:
+      self.optimizer.zero_grad()
+    out_batch = self.net(data_batch)
+    loss = self.criterion(out_batch, label_batch)
+    if optimize:
+      loss.backward()
+      self.optimizer.step()
 
-  return loss
+    return loss
 
-# set flags / seeds
-torch.backends.cudnn.benchmark = True
-torch.manual_seed(1)
-torch.cuda.manual_seed(1)
-USE_CUDA = torch.cuda.is_available()
+  def train(self):
+    train_dataset = HARWindows(self.TRAIN_SET_FILEPATH)
+    val_dataset = HARWindows(self.VAL_SET_FILEPATH)
+    test_dataset = HARWindows(self.TEST_SET_FILEPATH)
 
-if __name__ == '__main__':    
-  train_dataset = HARWindows(PAMAP2_TRAIN_SET_FILEPATH)
-  val_dataset = HARWindows(PAMAP2_VAL_SET_FILEPATH)
-  test_dataset = HARWindows(PAMAP2_TEST_SET_FILEPATH)
+    train_dataloader = DataLoader(train_dataset, batch_size=self.BATCH_SIZE, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=len(val_dataset), shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=len(test_dataset), shuffle=True)
+    
+    writer = SummaryWriter(self.LOGS_BASEPATH)
 
-  train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-  val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
-  test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
-  
-  net = SimpleCNN()
-  class_weights = torch.ones(NUM_CLASSES)                                         # TODO weight classes by appearance in dataset
-  
-  if USE_CUDA:
-    net = net.cuda()
-    class_weights = class_weights.cuda()
+    self.net = SimpleCNN().to(DEVICE)
+    class_weights = torch.ones(self.NUM_CLASSES, device=DEVICE)                                  # TODO weight classes by appearance in dataset
 
-  criterion = nn.CrossEntropyLoss(weight=class_weights)
-  optimizer = optim.RMSprop(net.parameters(), lr=LEARNING_RATE, alpha=RMS_DECAY)
-  
-  # typically we use tensorboardX to keep track of experiments
-  # writer = SummaryWriter(...)
-  
-  for epoch in range(PAMAP2_EPOCHS):
-    net.train()
+    self.criterion = torch.nn.CrossEntropyLoss(weight=class_weights)
+    self.optimizer = torch.optim.RMSprop(self.net.parameters(), lr=self.LEARNING_RATE, alpha=self.RMS_DECAY)
+    
+    # typically we use tensorboardX to keep track of experiments
+    # writer = SummaryWriter(...)
+    
+    for epoch in range(self.EPOCHS):
+      self.net.train()
 
-    pbar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
-    for i, data in pbar:
-      start_time = time.time()
-      loss = process_batch(data, net, criterion, optimizer)
-
-      # udpate tensorboardX
-      #writer.add_scalar(..., n_iter)
-
-      process_time = time.time() - start_time
-      pbar.set_description(f"Elapsed time: {process_time:.3f}s, loss: {loss.item()}, epoch: {epoch}/{PAMAP2_EPOCHS}:")
-          
-    # validation loss
-    net.eval()
-    with torch.no_grad():
-      pbar = tqdm(enumerate(val_dataloader), total=len(val_dataloader))
-      loss_acc = 0
+      pbar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
       for i, data in pbar:
         start_time = time.time()
-        loss = process_batch(data, net, criterion)
-        loss_acc += loss.item()
-        process_time = time.time() - start_time
-        pbar.set_description(f"Elapsed time: {process_time:.3f}s, loss: {loss.item()}, epoch: {epoch}/{PAMAP2_EPOCHS}:")
-      print(f"Epoch {epoch}/{PAMAP2_EPOCHS} validation loss: total: {loss_acc}, mean: {loss_acc / len(val_dataloader)}")
+        loss = self.process_batch(data)
 
-  torch.save(net.state_dict(), MODELS_BASEPATH + "first_model.pt")
+        # udpate tensorboardX
+        #writer.add_scalar(..., n_iter)
+
+        process_time = time.time() - start_time
+        pbar.set_description(f"loss: {loss.item()}, epoch: {epoch}/{self.EPOCHS}:")
+            
+      # validation loss
+      self.net.eval()
+      with torch.no_grad():
+        pbar = tqdm(enumerate(val_dataloader), total=len(val_dataloader))
+        loss_acc = 0
+        for i, data in pbar:
+          start_time = time.time()
+          loss = self.process_batch(data, optimize=False)
+          loss_acc += loss.item()
+          process_time = time.time() - start_time
+          pbar.set_description(f"loss: {loss.item()}, epoch: {epoch}/{self.EPOCHS}:")
+        print(f"Epoch {epoch}/{self.EPOCHS} validation loss: total: {loss_acc}, mean: {loss_acc / len(val_dataloader)}")
+
+  def evaluate(self, dataset):
+    pass  
