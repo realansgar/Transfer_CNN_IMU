3c961ad47cab014f20823a3754b5660233a42cb1
oskar.zeino <oskar.zeino@tu-dortmund.de>
2020-09-05 15:07:54 +0200

    Stuff gets plotted (but wrong formatting)

--- experiments.py
+++ experiments.py
@@ -1,5 +1,31 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 from train import Trainer
-from config import PAMAP2
+from config import PAMAP2, LOGS_BASEPATH, DEVICE
+
+def plot(filename):
+  eval_dict = torch.load(filename, map_location=DEVICE)
+  train_eval = eval_dict["train"]
+  val_eval = eval_dict["val"]
+  # [{"a": [1,2], "b": [3,4]}, {"a": [5,6], "b": [7,8]}] -> {"a": [1,2,5,6], "b": [3,4,7,8]} losing information about epochs
+  train_eval = {key: [item for sublist in [d[key] for d in train_eval] for item in sublist] for key in train_eval[0]}
+  val_eval = {key: [item for sublist in [d[key] for d in val_eval] for item in sublist] for key in val_eval[0]}
+  fig, axs = plt.subplots(len(train_eval) // 2, 2)
+  axs = axs.flatten()
+  for (i, key) in enumerate(train_eval):
+    ax = axs[i]
+    ax.plot(range(len(train_eval[key])), train_eval[key], label="train")
+    ax.plot(range(len(val_eval[key])), val_eval[key], label="validation")
+    ax.set_xlabel("iterations")
+    ax.set_ylabel(key)
+    ax.set_title(key)
+    ax.legend()
+  title = os.path.basename(filename)
+  #fig.set_title(title)
+  fig.savefig(os.path.splitext(filename)[0] + ".pdf", bbox_inches='tight')
+
 
 def pamap2_hyperparameters():
   pamap2_trainer = Trainer(PAMAP2)
--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from config import *
 
 if __name__ == "__main__":
   parser = ArgumentParser(description="Starts experiments and produces plots for those experiments.")
-  
+  parser.add_argument("action", choices=["experiment", "plot"], help="what you want to do")
+  parser.add_argument("filename")
   args = parser.parse_args()
 
-  experiments.pamap2_hyperparameters()
+  if args.action == "experiment":
+    experiments.pamap2_hyperparameters()
+  elif args.action == "plot":
+    experiments.plot(args.filename)
